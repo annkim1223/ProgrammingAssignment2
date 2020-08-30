1a221cfb835f8f54787252dd5d20289290057f0c
Jaeyoung Kim <40916751+annkim1223@users.noreply.github.com>
2020-08-29 22:15:50 -0400

    edit cashematrix.R with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Comments: The following functions compute the inverse of the matrix 
+##          and cash the data so that the computation is not repeated.
+
 ## Write a short comment describing this function
-
+## Description: makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    INV <- NULL
+    set <- function(y) {
+        x <<- y
+        INV <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) INV <<- inverse
+    getinverse <- function() INV
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Description: 
+##      cacheSolve function computes the inverse of the special "matrix" returned 
+##      by makeCacheMatrix above. If the inverse has already been calculated 
+##      (and the matrix has not changed), then the cachesolve should retrieve 
+##      the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    INV <- x$getinverse()
+    if(!is.null(INV)) {
+        message("getting cached data")
+        return(INV)
+    }
+    data <- x$get()
+    INV <- solve(data, ...)
+    x$setinverse(INV)
+    INV
 }
+
+}
+
+
+#Example
+exmatrix <- matrix( c(5, 1, 0,
+                      3,-1, 2,
+                      4, 0,-1), nrow=3, byrow=TRUE)
+
+cacheSolve(makeCacheMatrix(exmatrix))
